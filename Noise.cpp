7ba3fcad3dda9523496ae7f3c7900b7a737a05d0
Bavfalcon9 <olybear9@gmail.com>
2021-05-18 17:10:58 -0500

    feat: Add generate3d(x,y,z)

--- Noise.cpp
+++ Noise.cpp
@@ -46,14 +46,51 @@ PHP_METHOD(noiselib, generate2d) {
     RETVAL_ARR(data);
 }
 // }} generate3d {{
+ZEND_BEGIN_ARG_INFO(arginfo_gen3d, 1)
+    ZEND_ARG_INFO(1, x)
+    ZEND_ARG_INFO(1, y)
+    ZEND_ARG_INFO(1, z)
+    ZEND_ARG_INFO_WITH_DEFAULT_VALUE(1, x_off, 0)
+    ZEND_ARG_INFO_WITH_DEFAULT_VALUE(1, y_off, 0)
+    ZEND_ARG_INFO_WITH_DEFAULT_VALUE(1, z_off, 0)
+    ZEND_ARG_INFO_WITH_DEFAULT_VALUE(1, amplifier, 0)
+ZEND_END_ARG_INFO();
+
+PHP_METHOD(noiselib, generate3d) {
+    FastNoiseLite noise = FastNoiseLite();
+    zend_long x, y, z, x_off, y_off, z_off, amplifier;
+    ZEND_PARSE_PARAMETERS_START(2, 4)
+        Z_PARAM_LONG(x)
+        Z_PARAM_LONG(y)
+        Z_PARAM_LONG(z)
+        Z_PARAM_LONG(x_off)
+        Z_PARAM_LONG(y_off)
+        Z_PARAM_LONG(z_off)
+        Z_PARAM_LONG(amplifier)
+    ZEND_PARSE_PARAMETERS_END();
+
+    zend_array *data = zend_new_array(0);
+    zend_ulong idx = 0;
+    for (int xx = x_off; xx < (x + x_off); x++) {
+        for (int yx = y_off; yx < (y + y_off); yx++) {
+            for (int zx = z_off; zx < (z + z_off); zx++) {
+                double nsx = (double) noise.GetNoise((float) xx, (float) zx);
+                zval *val;
+                ZVAL_DOUBLE(val, nsx);
+                zend_hash_index_add(data, idx++, val);
+            }
+        }
+    }
+    RETVAL_ARR(data);
+}
 // }}
 
 // Register methods to class
 static zend_function_entry noise_lib_methods[] = {
     PHP_ME(noiselib, generate2d, arginfo_gen2d, ZEND_ACC_PUBLIC)
+    PHP_ME(noiselib, generate3d, arginfo_gen3d, ZEND_ACC_PUBLIC)
     PHP_FE_END
 };
-
 
 void register_noise_class() {
     zend_class_entry cle;
