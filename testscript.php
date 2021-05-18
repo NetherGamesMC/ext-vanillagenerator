93422b1e7d1a946bfa3762287e9f6bb8add3de08
Bavfalcon9 <olybear9@gmail.com>
2021-05-18 13:37:56 -0500

    chore: fix test script

--- testscript.php
+++ testscript.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 $noise = new Noise();
 $noise->initialize();
 $start = hrtime(true);
-$noise->generate(32, 32);
+$noise->generate2d(32, 32);
 var_dump(hrtime(true) - $start);
 $noise->free();
 ?>
